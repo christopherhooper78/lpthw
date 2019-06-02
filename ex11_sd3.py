a69d61aa53f4c782f5c0baf88db62dd6d89c8401
christopher.hooper78 <christopher.hooper78@protonmail.com>
2019-06-02 18:33:36 +0100

    second commit

---
+++ ex11_sd3.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+colour = input("What is your favorite colour? ")
+
+print(f"So, your name is {name}, your quest is {quest} and your favorite colour is {colour}.")
---
+++ ex13_sd3.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first = argv
+
+input = input("Input a string: ")
+
+print(first + " " + input)
