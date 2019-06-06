8112840756aa46c0260c2f1d36ac4a97e84e68f7
christopher.hooper78 <christopher.hooper78@protonmail.com>
2019-06-06 21:50:27 +0100

    sixth commit

--- ex23.py
+++ ex23.py
@@ -10,10 +10,10 @@ def main(language_file, encoding, errors):
         return main(language_file, encoding, errors)
 
 
-def print_line(line, encoding, erros):
+def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, erros=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
 
